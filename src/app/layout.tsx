6b1f6a5128428fc7d1009bdb5d46de8384faffaa
JasonLam1218 <878jason878@gmail.com>
2025-07-10 18:08:17 +0800

    test for deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
-import { Inter } from 'next/font/google';
-import ThemeRegistry from '@/components/ThemeRegistry';
 import './globals.css';
+import { Inter } from 'next/font/google'
 
 const inter = Inter({ 
   subsets: ['latin'],
